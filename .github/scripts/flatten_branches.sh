f1428e58f7dff6ff0291501c451c593f8bad11a8
Drew Kilb <drew.kilb@duke.edu>
2021-04-20 17:34:14 -0400

    debug: print out git log

--- .github/scripts/flatten_branches.sh
+++ .github/scripts/flatten_branches.sh
@@ -8,7 +8,8 @@ find ./src/content -type f -name '*.md' -or -name '*.njk' \
 cd ./src/content || exit
 for branch in $(git for-each-ref --format='%(refname)' refs/heads/); do
     if [ "$branch" != 'refs/heads/main' ]; then
-        branch="$(echo $branch | cut -d'/' -f 3)"
-        git merge -m 'Automerge' "$branch"
+        short_branch="$(echo "$branch" | cut -d'/' -f 3)"
+        git merge -m 'Automerge' "$short_branch"
     fi
 done
+git log --oneline
